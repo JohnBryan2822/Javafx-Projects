b4c8a57b5edec6857aadf13bd0eeacb63046e0e9
JohnBryan2822 <johnbryan2822@gmail.com>
2023-10-31 14:02:58 +0100

    Web Cam in FX

---
+++ WebCamTutorial/src/module-info.java
@@ -0,0 +1,9 @@
+module WebCamTutorial {
+    requires java.desktop;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.controls;
+	requires webcam.capture;
+	
+	exports org.javacoders.webcamtutorial;
+}
---
+++ WebCamTutorial/src/org/javacoders/webcamtutorial/WebcamPanelTutorial.java
@@ -0,0 +1,31 @@
+package org.javacoders.webcamtutorial;
+
+import javax.swing.JFrame;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+
+public class WebcamPanelTutorial {
+	
+	public static void main(String[] args) {
+		
+		Webcam webcam = Webcam.getDefault();
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+		
+		WebcamPanel webcamPanel = new WebcamPanel(webcam);
+		webcamPanel.setImageSizeDisplayed(true);
+		webcamPanel.setFPSDisplayed(true);
+		webcamPanel.setMirrored(true);
+		webcamPanel.setDisplayDebugInfo(true);
+		
+		JFrame frame = new JFrame();
+		frame.add(webcamPanel);
+		frame.setLocationRelativeTo(null);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+
+}
