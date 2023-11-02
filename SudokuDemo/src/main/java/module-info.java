b5268dee5ed1d7d3ac8b749d0828a73bd07a3e09
JohnBryan2822 <johnbryan2822@gmail.com>
2023-11-02 13:08:27 +0100

    Sudoku in FX - Commputation Logic

--- SudokuDemo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.javacoders.SudokuDemo {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires javafx.graphics;
-
-    opens org.javacoders.SudokuDemo to javafx.fxml;
-    exports org.javacoders.SudokuDemo;
-}
